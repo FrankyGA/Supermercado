aa9eef59ce740723e6ce3ec4e201f2866ef7d3b7
Fran <124530921+FrankyGA@users.noreply.github.com>
2024-08-28 13:21:00 +0200

    Programa completo

---
+++ src/supermercado/ConDescuento.java
@@ -0,0 +1,9 @@
+package supermercado;
+
+public interface ConDescuento {
+	
+	public void setDescuento(double des);
+    public double getDescuento();
+    public double getPrecioDescuento();
+
+}
---
+++ src/supermercado/EsLiquido.java
@@ -0,0 +1,10 @@
+package supermercado;
+
+public interface EsLiquido {
+	
+	public void setVolumen(double v);
+    public double getVolumen();
+    public void setTipoEnvase(String env);
+    public String getTipoEnvase();
+
+}
